9d1678e9ad15f2079e73d414c053ba1c4b2a39ab
Kauany Miranda <kauany.m.dantas@gmail.com>
2024-10-24 00:25:22 -0400

    Inicio do Projeto

---
+++ Pessoa.cs
@@ -0,0 +1,39 @@
+﻿
+public class Pessoa
+{
+    public string nome;
+    public int cpf;
+    public int idade;
+    public string sexo;
+    public double peso;
+    public double altura;
+    public double valorIMC;
+
+    public void CalcularIMC(double peso, double altura)
+    {
+        valorIMC = peso / (altura * altura);
+
+        if (valorIMC <= 18.5)
+        {
+            Console.WriteLine($"Valor IMC = {valorIMC.ToString("F2")} MAGREZA");
+        }
+        if (valorIMC >= 18.5 & valorIMC <= 24.9)
+        {
+            Console.WriteLine($"Valor IMC = {valorIMC.ToString("F2")} NORMAL");
+        }
+        if (valorIMC >= 25.0 & valorIMC <= 29.9)
+        {
+            Console.WriteLine($"Valor IMC = {valorIMC.ToString("F2")} SOBREPESO GRAU I");
+        }
+        if (valorIMC >= 30.0 & valorIMC <= 39.9)
+        {
+            Console.WriteLine($"Valor IMC = {valorIMC.ToString("F2")} OBESIDADE GRAU II");
+        }
+        if (valorIMC >= 40.00)
+        {
+            Console.WriteLine($"Valor IMC = {valorIMC.ToString("F2")} OBESIDADE GRAU III");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.nome = "Kauany";
+        pessoa.cpf = 05555;
+        pessoa.idade = 20;
+        pessoa.sexo = "Fem";
+
+
+        Console.Write("Digite o peso: ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a altura: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        pessoa.CalcularIMC(peso, altura);
+    }
+}
